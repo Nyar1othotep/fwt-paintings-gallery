e4dc74188963af72eb78e4412448fb81ee3f33a3
nyar1othotep <freelancerart@yandex.ru>
2023-09-29 14:34:04 +0400

    feat: add ThemeToggle

---
+++ src/features/theme/ui/ToggleTheme/ToggleTheme.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import cn from "classnames";
+
+// Entities
+import { changeTheme, selectTheme } from "entities/theme";
+
+// Shared
+import { useAppDispatch, useAppSelector } from "shared/lib";
+
+import { ReactComponent as Icon } from "../assets/theme.svg";
+import styles from "./ToggleTheme.module.scss";
+
+type TypeProps = {
+  className: string;
+};
+
+export function ToggleTheme({ className }: TypeProps) {
+  const dispatch = useAppDispatch();
+  const theme = useAppSelector(selectTheme);
+
+  const handleClick = () => {
+    dispatch(changeTheme(theme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <div className={cn(styles.root, className)}>
+      <button className={styles.wrapper} onClick={handleClick} type="button">
+        <Icon className={styles.svg} />
+      </button>
+    </div>
+  );
+}
--- src/widgets/header/ui/HeaderLayout/HeaderLayout.tsx
+++ src/widgets/header/ui/HeaderLayout/HeaderLayout.tsx
@@ -1,34 +1,21 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 
-// Entities
-import { changeTheme, selectTheme } from "entities/theme";
-
-// Shared
-import { useAppSelector } from "shared/lib";
+// Features
+import { ToggleTheme } from "features/theme";
 
 import styles from "./HeaderLayout.module.scss";
 
 export function HeaderLayout() {
-  const dispatch = useDispatch();
-  const theme = useAppSelector(selectTheme);
-
-  const handleClick = () => {
-    dispatch(changeTheme(theme === "light" ? "dark" : "light"));
-  };
-
   return (
     <header className={styles.root}>
-      <a href="/">
+      <a href="https://framework.team/">
         <img
           className={styles.logo}
           src="/fwt-paintings-gallery/images/logo.png"
           alt="Logo FWT-Framework team"
         />
       </a>
-      <button type="button" onClick={handleClick}>
-        Toggle theme
-      </button>
+      <ToggleTheme className={styles.theme} />
     </header>
   );
 }
